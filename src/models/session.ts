f2c90ba93a87638a08c58e1505e84869de35157d
Samuel-Amaro <Samuelamaro96746313@gmail.com>
2025-08-30 14:10:23 -0300

    feat: implement `/api/v1/user` endpoint with session renewal (#10)
    * feat: implement `/api/v1/user` endpoint with session renewal
    
    * test: add novo test, verificaação de renovação de sessão na metade da vida

--- src/models/session.ts
+++ src/models/session.ts
@@ -1,6 +1,7 @@
 import database from '../../infra/database';
 import crypto from 'node:crypto';
 import type { TypeSessions } from '../types/sessions';
+import { NotFoundError, UnauthorizedError } from '../../infra/errors';
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; //30 dias
 
@@ -28,9 +29,76 @@ async function create(idUser: string) {
 	}
 }
 
+async function findOneValidByToken(token: string | undefined) {
+	if (token) {
+		return await runSelectQuery(token);
+	} else {
+		throw new NotFoundError({
+			message: 'Token de sessão não informado.',
+			action: 'Informe um token de sessão válido e tente novamente.',
+			cause: 'function findOneByValidByToken in model session'
+		});
+	}
+
+	async function runSelectQuery(token: string) {
+		const results = await database.query({
+			text: `
+      SELECT
+        *
+      FROM
+        sessions
+      WHERE
+        token = $1
+      AND
+        expires_at > NOW()
+      LIMIT
+        1
+      ;`,
+			values: [token]
+		});
+
+		if (results.rowCount === 0) {
+			throw new UnauthorizedError({
+				message: 'Usuário não possui sessão ativa.',
+				action: 'Verifique se este usuário está logado e tente novamente.',
+				cause: 'function findOneByValidByToken in model session'
+			});
+		}
+
+		return results.rows[0] as TypeSessions;
+	}
+}
+
+async function renew(id: string) {
+	const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+	return await runUpdateQuery(id, expiresAt);
+
+	async function runUpdateQuery(idSession: string, expiresAt: Date) {
+		const results = await database.query({
+			text: `
+      UPDATE
+        sessions
+      SET
+        expires_at = $2,
+        updated_at = NOW()
+      WHERE
+        id = $1
+      RETURNING
+        *
+      ;`,
+			values: [idSession, expiresAt]
+		});
+
+		return results.rows[0] as TypeSessions;
+	}
+}
+
 const session = {
 	create,
-	EXPIRATION_IN_MILLISECONDS
+	EXPIRATION_IN_MILLISECONDS,
+	findOneValidByToken,
+	renew
 };
 
 export default session;
---
+++ src/routes/api/v1/user/+server.ts
@@ -0,0 +1,24 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import controller from '../../../../../infra/controller';
+import session from '../../../../models/session';
+import user from '../../../../models/user';
+
+export const GET: RequestHandler = async ({ cookies }) => {
+	try {
+		const sessionToken = cookies.get('session_id');
+
+		const sessionObject = await session.findOneValidByToken(sessionToken);
+		const renewedSessionObject = await session.renew(sessionObject.id);
+
+		const userFound = await user.findOneById(sessionObject.user_id);
+
+		return json(userFound, {
+			headers: {
+				'Set-Cookie': `session_id=${renewedSessionObject.token}; Path=/; Max-Age=${session.EXPIRATION_IN_MILLISECONDS / 1000};${import.meta.env.MODE === 'production' ? ' Secure=true;' : ''} HttpOnly=true;`,
+				'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0'
+			}
+		});
+	} catch (error) {
+		return controller.onErrorHandler(error);
+	}
+};
